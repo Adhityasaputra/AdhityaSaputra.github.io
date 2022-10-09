5b10bdadce8284145bb3633dc03f5d3c1dfab26d
Adhityasaputra <113341380+Adhityasaputra@users.noreply.github.com>
2022-10-09 23:30:18 +0800

    postest4

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="form.css">
+    
+</head>
+<body>
+    <div class="form">
+        <h1 align="center">FORM PENDATAAN</h1>
+
+    </div>
+</body>
+</html>
